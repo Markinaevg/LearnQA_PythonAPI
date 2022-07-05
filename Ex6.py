4e39fe712ca4ce2b0c0ff58529bd0e6c7db1361a
Markinaevg <107902519+Markinaevg@users.noreply.github.com>
2022-07-05 15:44:56 +1000

    Add files via upload

--- Ex6.py
+++ Ex6.py
@@ -1,10 +1,8 @@
 import requests
 
 response = requests.post(" https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-last_response = response
-
 count = len(response.history)
 
 print(f"Количество редиректов - {count}")
-print(f"Итоговый редирект - {last_response.url}")
+print(f"Итоговый редирект - {response.url}")
 
