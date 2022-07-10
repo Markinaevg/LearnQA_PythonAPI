0c0166ddbc4fb93872d47ede8b2a10a4e480ec02
Markinaevg <107902519+Markinaevg@users.noreply.github.com>
2022-07-10 21:37:17 +1000

    Add files via upload

--- Ex12.py
+++ Ex12.py
@@ -5,5 +5,9 @@ class Test_EX12:
         response = requests.get("https://playground.learnqa.ru/api/homework_header")
         headers = response.headers
         print(f"Содержимое headers: {headers}")
+
+        assert response.status_code == 200, "Wronge response code"
+        assert "x-secret-homework-header" in headers, "There field 'x-secret-homework-header' in the response"
+
         assert headers["x-secret-homework-header"] == "Some secret value", f'Некорректное значение Headers. Должно быть - {headers["x-secret-homework-header"]}'
-        print(f'Значение {headers["x-secret-homework-header"]} указано верно')
+        print(f'Значение "{headers["x-secret-homework-header"]}" указано верно')
