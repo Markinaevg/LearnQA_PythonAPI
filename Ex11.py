22c19200c2bd8abae66179e6df2a4575ccecc388
Markinaevg <107902519+Markinaevg@users.noreply.github.com>
2022-07-10 21:32:37 +1000

    Add files via upload

--- Ex11.py
+++ Ex11.py
@@ -5,4 +5,9 @@ class Test_EX11:
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
         cookie = response.cookies
         print(f"Содержимое cookie: {cookie}")
+
+        assert response.status_code == 200, "Wronge response code"
+        assert "HomeWork" in cookie, "There field 'HomeWork' in the response"
+
         assert cookie["HomeWork"] == "hw_value", "Некорректное значение Cookie"
+
