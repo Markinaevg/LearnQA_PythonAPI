be791ad438d933cd95aa70e540815b082578da96
Markinaevg <107902519+Markinaevg@users.noreply.github.com>
2022-07-18 23:58:41 +1000

    Add files via upload

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -46,10 +46,10 @@ class TestUserGet(BaseCase):
 
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
-        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+        second_user_id = self.get_json_value(response1, "user_id") - 1
 
         response2 = MyRequests.get(
-            f"/user/{user_id_from_auth_method}",
+            f"/user/{second_user_id}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid}
         )
